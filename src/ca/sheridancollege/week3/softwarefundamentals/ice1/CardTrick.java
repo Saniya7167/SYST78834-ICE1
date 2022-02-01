0e544e45eb7c831ddad2af230e4963bc039da0b8
saniy <saniy@DESKTOP-QBDDNEN>
2022-02-01 00:07:19 -0500

    Updated Author

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -9,21 +9,22 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author dancye
- * @modified by Megha Patel
- * 
+ *
+ * @author Saniya Hussain
+ * @modified by Saniya Hussain
+ *
  */
-public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-             }
-        
-         }
-    
+public class CardTrick
+{
+
+   public static void main (String[] args)
+   {
+      Card[] magicHand = new Card[7];
+
+      for (int i = 0; i < magicHand.length; i++) {
+         Card c = new Card();
+      }
+
+   }
+
 }
